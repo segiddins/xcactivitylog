07acfe62d5636b4c0992a1dfbae9baf58e8981e1
Nick DiZazzo <ndizazzo@squareup.com>
2020-12-03 16:41:38 -0500

    Don't throw an exception when Xcode generates an unprocessable archive

--- lib/xcactivitylog.rb
+++ lib/xcactivitylog.rb
@@ -12,5 +12,7 @@ module XCActivityLog
     unzipped_contents = Zlib.gunzip(contents)
 
     Parser.new(class_deserializer: {}).parse!(unzipped_contents)
+  rescue Zlib::GzipFile::Error => e
+    puts "ERROR: Unable to parse xcactivity log at path #{path} - #{e.message}. Skipping."
   end
 end
